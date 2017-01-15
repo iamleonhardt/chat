a2d0776c15f4d53dbd1365d85c52be64aa2429d9
Bill Leonhardt <iamleonhardt@gmail.com>
2017-01-15 14:40:38 -0800

    socketio

--- server.js
+++ server.js
@@ -11,15 +11,11 @@ var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
 
-
-
-
 users = [];
 connections = [];
 
 
-
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 80);
 console.log('Server started');
 
 app.get('/', function(req, res){
